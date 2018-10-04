d8b3818cf0ee0115c0247305f908ef1b2d95601a
Karl Benbeneck <kbenbeneck@gmail.com>
2018-10-04 02:43:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,4 +5,8 @@ def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   end
+  if int % 5 == 0 # if the number int is divisible by 3
+    "Buzz" # Go fizz
+  end
 end
+
